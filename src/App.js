6e5f29df481863ac2379a0b03a9a01ac71c7e2c4
Gregory Kysenko <heyapple1395@gmail.com>
2018-05-16 02:04:37 +0300

    Initial state for inCode project

---
+++ src/App.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import Projects from './components/Projects.js';
+import axios from 'axios';
+import './App.css';
+
+
+class App extends Component {
+  constructor(){
+    super();
+    this.state = {
+         items: [],
+   }
+   axios.get('https://raw.githubusercontent.com/CodeNinja1395/InCode-test-project/master/clients.json')
+    .then(res => {
+        this.setState({ items: res.data });
+   });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        myApp
+        <Projects items={this.state.items} />
+      </div>
+    );
+  }
+
+}
+
+
+export default App;
---
+++ src/components/Projects.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+//import Projects from './components/Projects.js';
+
+
+class Projects extends Component {
+
+  render() {
+    console.log(this.props);
+    return (
+      <div className="Projects">
+        my Projects
+      </div>
+    );
+  }
+}
+
+export default Projects;
